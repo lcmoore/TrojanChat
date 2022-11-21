649b8298f9583a0f71dcef388f254cb1f4ec6760
lcmoore <72061542+lcmoore@users.noreply.github.com>
2022-11-20 20:33:28 -0800

    Basic Working Model. Prior to implementation of interface for data retreival

--- TrojanChat/MVVM/ViewModel/MainViewModel.cs
+++ TrojanChat/MVVM/ViewModel/MainViewModel.cs
@@ -4,25 +4,67 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TrojanChat.Core;
 using TrojanChat.MVVM.Model;
 
 namespace TrojanChat.MVVM.ViewModel
 {
-    class MainViewModel
+    class MainViewModel : ObservableObject
     {
         public ObservableCollection<MessageModel> Messages { get; set; }
         public ObservableCollection<ContactModel> Contacts { get; set; }
+
+        //Commands
+        public RelayCommand SendCommand { get; set; }
+
+
+        private ContactModel _selectedContact;
+
+        public ContactModel SelectedContact
+        {
+            get { return _selectedContact; }
+            set { _selectedContact = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private string _message;
+
+        public string Message
+        {   
+            get { return _message; }
+            set { 
+                _message = value;
+                OnPropertyChanged();
+
+            }
+        }
+
         public MainViewModel()
         {
             Messages = new ObservableCollection<MessageModel>();
             Contacts = new ObservableCollection<ContactModel>();
 
+            SendCommand = new RelayCommand(o =>
+            {
+                Messages.Add(new MessageModel
+                {
+                    MessageText = Message,
+                    IsFirstMessage = false
+                });
+
+                Message = "";
+
+            });
+
+
             //Here is where data population will occur
             Messages.Add(new MessageModel
             {
                 Username = "Test User",
-                UsernameColor = "#409aff",
-                //ImageSource = "https://i.imgur.com/yMWvLXd.png",
+                UsernameColor = "White",
+                ImageSource = "https://i.imgur.com/yMWvLXd.png",
                 MessageText = "This is the message text",
                 TimeStamp = DateTime.Now,
                 IsFirstMessage = true,
@@ -34,8 +76,8 @@ namespace TrojanChat.MVVM.ViewModel
                 Messages.Add(new MessageModel
                 {
                     Username = "Test User Self",
-                    UsernameColor = "#409aff",
-                    //ImageSource = "https://i.imgur.com/yMWvLXd.png",
+                    UsernameColor = "White",
+                    ImageSource = "https://i.imgur.com/yMWvLXd.png",
                     MessageText = "This is the message text",
                     TimeStamp = DateTime.Now,
                     IsFirstMessage = false,
@@ -48,8 +90,8 @@ namespace TrojanChat.MVVM.ViewModel
             {
                 Contacts.Add(new ContactModel
                 {
-                    UserName = $" Contact Number {i}",
-                    //ImageSource = "https://i.imgur.com/yMWvLXd.png",
+                    UserName = $" Chat Number {i}",
+                    ImageSource = "https://i.imgur.com/yMWvLXd.png",
                     MessageHistory = Messages
                 });
             }
